c65e9911db87422a599040dfc66bcc9a40831d9f
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-05-21 20:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ function indexOfIgnoreCase(s1, s2) {
 }
 
 // // Please do not change the code below
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-// alert(indexOfIgnoreCase(s1, s2));
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(indexOfIgnoreCase(s1, s2));
+
+// function findFirstMatchIndex(str, substr) {
+//   const strLen = str.length;
+//   const subLen = substr.length;
+
+//   for (let i = 0; i <= strLen - subLen; i++) {
+//     let match = true;
+//     for (let j = 0; j < subLen; j++) {
+//       if (str[i + j] !== substr[j]) {
+//         match = false;
+//         break;
+//       }
+//     }
+//     if (match) {
+//       return i;
+//     }
+//   }
+
+//   return -1; // not found
+// }
